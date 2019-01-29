22ee977c62ba04e5aa5bc0b934804a47683e7201
Vijay <vijay.sankeerth1@gmail.com>
2019-01-29 21:52:45 +0530

    Notification spec

--- spec/finding_falcone/NotificationSpec.js
+++ spec/finding_falcone/NotificationSpec.js
@@ -8,16 +8,31 @@ var Validation = app.__get__('Validation');
 var Time = app.__get__('Time');
 
 describe("Validation: ", function() {
-  it("It should give error", function() {
-    Validation.validate();
+  it("Positive case", function() {
+    expect(Validation.validate()).toBe(false);
   });
-  it("It should give error", function() {
-    Validation.validate([]);
+
+  it("Positive case", function() {
+    var array = [];
+    expect(Validation.validate(array)).toBe(false);
+  });
+
+  it("It should give success", function() {
+    var array = [1,2,3];
+    expect(Validation.validate(array)).toBe(false);
   });
   it("It should give success", function() {
-    Validation.validate([1,2,3]);
+    var array = [{}, "" , 123];
+    expect(Validation.validate()).toBe(false);
   });
-  it("It should give success", function() {
-    Validation.validate([{}, {}, ""]);
+
+  it("Positive case", function() {
+    var array = ["One", "Two", "Three", "Four"];
+    expect(Validation.validate(array)).toBe(true);
+  });
+
+  it("Positive case", function() {
+    var array = [1, "Two", "Three", "Four"];
+    expect(Validation.validate(array)).toBe(false);
   });
 });
